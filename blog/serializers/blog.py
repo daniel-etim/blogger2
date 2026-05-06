bf504b2afe4b79dd61cec9e119e731fce0e8dcfd
Daniel Etim <detimworks@gmail.com>
2026-05-06 18:04:40 +0100

    commit .gitignore files

--- blog/serializers/blog.py
+++ blog/serializers/blog.py
@@ -1,10 +1,10 @@
-from django.db import models
 from rest_framework import serializers
 
 from blog.models.blog import Post
 
 
 class PostSerializer(serializers.ModelSerializer):
+    author = serializers.StringRelatedField()
     class Meta:
         model = Post
         fields = "__all__"
