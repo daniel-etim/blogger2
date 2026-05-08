d45ca2bbf1b70f088442882c7f07d57aecc0696e
Daniel Etim <detimworks@gmail.com>
2026-05-08 16:50:36 +0100

    fix: uniqe slug

--- blog/models/blog.py
+++ blog/models/blog.py
@@ -7,7 +7,7 @@ from user.models.user import User
 class Post(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
-    slug = models.SlugField()
+    slug = models.SlugField(unique=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, blank=True, null=True)
