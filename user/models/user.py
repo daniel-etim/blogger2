0b9997183e1114ad3845610863470fa7222e9d65
Daniel Etim <detimworks@gmail.com>
2026-05-16 19:56:51 +0100

    refactor: refactor user registration
    first & second name are optional for the time being

--- user/models/user.py
+++ user/models/user.py
@@ -3,8 +3,6 @@ from django.db import models
 
 class User(AbstractUser):
     email = models.EmailField(unique=True, null=False, blank=False)
-    first_name = models.CharField(max_length=100)
-    last_name = models.CharField(max_length=100)
 
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = ["username"]
--- user/serializers/user.py
+++ user/serializers/user.py
@@ -1,19 +1,22 @@
+import re
+
 from rest_framework import serializers
 
 from user.models.user import User
 
 
 class RegisterModelSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(min_length=8, write_only=True)
+    password = serializers.CharField(write_only=True, min_length=8, max_length=128, required=True)
+
     class Meta:
         model = User
         fields = ['first_name', 'last_name', 'username', 'email', 'password']
 
     def create(self, validated_data):
         return User.objects.create_user(
+            first_name=validated_data.get("first_name", ""),
+            last_name=validated_data.get("last_name", ""),
             username=validated_data["username"],
-            first_name=validated_data["first_name"],
-            last_name=validated_data["last_name"],
             email=validated_data["email"],
             password=validated_data["password"],
         )
